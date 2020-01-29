3a0f92786abae8c992e9a15936256ccb93ebee6d
Jayachandran Jagannathan <jaya84@outlook.com>
2020-01-29 23:54:52 +0530

    ADDED: TokenAuth sample

---
+++ TokenAuth/Pages/LoginIDP.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TokenAuth.Pages
+{
+    public class LoginIDPModel : PageModel
+    {
+        #region Publics
+        public async Task OnGetAsync()
+        {
+            if(!this.HttpContext.User.Identity.IsAuthenticated)
+            {
+                await this.HttpContext.ChallengeAsync(OpenIdConnectDefaults.AuthenticationScheme);
+            }
+            else
+            {
+                // redirect to the root
+                this.Response.Redirect(this.Url.Content("~/"));
+            }
+        }
+        #endregion
+    }
+}
---
+++ TokenAuth/Pages/LogoutIDP.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TokenAuth.Pages
+{
+    public class LogoutIDPModel : PageModel
+    {
+        #region Publics
+        public async Task OnGetAsync()
+        {
+            await this.HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            await this.HttpContext.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme);
+        }
+        #endregion
+    }
+}
